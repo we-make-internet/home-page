3b38fc2d73d305f1f4df50951e8be4c2c8a30529
Alexander Petros <contact@alexpetros.com>
2024-07-18 19:22:47 -0400

    Change location

--- www/switch-emoji/user.php
+++ www/switch-emoji/user.php
@@ -8,7 +8,7 @@ if ($req['method'] == 'POST'):
 
 $issue_date = new DateTime($issue_date_str);
 
-file_put_contents('submissions.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
+file_put_contents('./submissions.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
 
 header("Location: /switch-emoji/success");
 http_response_code(303);
