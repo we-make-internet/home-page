53b306b3ca6acc29ccb9412eb1caf8b734dfaf4d
Alexander Petros <contact@alexpetros.com>
2024-07-18 19:20:07 -0400

    Add form

---
+++ www/switch-emoji/user.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($req['method'] == 'POST'):
+[
+  'sequences' => $sequences,
+  'info' => $info
+] = $_POST;
+
+$issue_date = new DateTime($issue_date_str);
+
+file_put_contents('submissions.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
+
+header("Location: /switch-emoji/success");
+http_response_code(303);
+
+?>
