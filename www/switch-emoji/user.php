f80270764b1609802767a8664df9fd3a9ffbff9c
Alexander Petros <contact@alexpetros.com>
2024-07-18 19:35:10 -0400

    Save to file

--- www/switch-emoji/user.php
+++ www/switch-emoji/user.php
@@ -6,8 +6,14 @@ if ($req['method'] == 'POST'):
 ] = $_POST;
 
 $issue_date = new DateTime($issue_date_str);
+$txt = "
+$sequences
 
-file_put_contents('./submissions.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
+$info
+-------------^^^----
+"
+
+file_put_contents('/var/www/submissions.txt', $txt.PHP_EOL, FILE_APPEND | LOCK_EX);
 
 header("Location: /switch-emoji/success");
 http_response_code(303);
