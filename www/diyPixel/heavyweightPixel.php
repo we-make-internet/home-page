613671cd977085e9b5ada1983c6f77224e23d3d6
Morry Kolman <wttdotm@gmail.com>
2025-05-15 16:57:10 -0400

    added heavyweight pixel

---
+++ www/diyPixel/heavyweightPixel.php
@@ -0,0 +1,22 @@
+<?php
+
+//THIS RETURNS THE IMAGE
+header('Content-Type: image/gif');
+//this is non-caching stuff
+header("Pragma: no-cache"); //HTTP 1.0
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+readfile('pixel.gif');
+
+// Get other info
+$ip = $_SERVER['REMOTE_ADDR'];
+$referringUrl = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Direct';
+$queryParams = isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : 'No query parameters';
+
+
+//THIS IS THE SCRIPT FOR THE ACTUAL TRACKING
+$date = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
+$txt = $date.",". $ip.",". $referringUrl.",". $queryParams;
+$myfile = file_put_contents('/var/www/heavyweightLog.txt', $txt.PHP_EOL , FILE_APPEND);
+exit;
+
+?>
